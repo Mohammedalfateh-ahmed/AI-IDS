d699a904f7509196c514756d3987f13b97b8bcc4
Mohammedalfateh-ahmed <mohammedalfateh.ahme@final.edu.tr>
2026-05-31 22:07:33 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,9 @@ from feature_extraction import extract_features_from_packet, generate_attack_fea
 from intelligence_engine import intelligence_engine
 from datetime import datetime, timedelta, timezone
 from smart_ips_system import smart_ips
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from behavior_analyzer import behavior_analyzer
 Path("logs").mkdir(exist_ok=True)
 def is_ips_enabled():
